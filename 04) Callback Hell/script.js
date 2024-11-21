97546226e21bd058aebfacdeabb3572f5d9a1816
Hrithik <hey.its.me.hrithik@gmail.com>
2024-11-21 10:30:55 +0530

    Modify script.js file for chapter 4 :- Callback Hell

--- 04) Callback Hell/script.js
+++ 04) Callback Hell/script.js
@@ -2,7 +2,7 @@ const btn2 = document.querySelector("#btn2")
 
 btn2.addEventListener("click", () => {
   makeHttpRequest("GET", "https://dummyjson.com/users", (all_user_data) => {
-    makeHttpRequest("GET", 'https://dummyjson.com/users/', (user_data) => {
+    makeHttpRequest("GET", `https://dummyjson.com/users/${all_user_data.users[5].id}`, (user_data) => {
       makeHttpRequest("GET", `https://dummyjson.com/posts/${user_data.id}`, (post_data) => {
         makeHttpRequest("GET", `https://dummyjson.com/comments/${post_data.id}`, (comment_data) => {
           console.log(comment_data.user.fullName);
