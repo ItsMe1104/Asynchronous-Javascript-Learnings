3b9cfce43786f93b65471a966afc053f7a0fa35d
Hrithik <hey.its.me.hrithik@gmail.com>
2024-11-25 14:23:31 +0530

    Modify index.js file of chapter 9 :- try catch in JS

--- 09) try catch/index.js
+++ 09) try catch/index.js
@@ -7,3 +7,20 @@ catch (err) {
 }
 
 console.log(39);
+
+
+
+async function makeAsyncRequest() {
+  try {
+    const resObj = await fetch("https://dummyjson.com/users")
+    const data = await resObj.json();
+    console.log(data);
+  }
+  catch (err) {
+    console.log(err);
+    alert("Something went Wrong !!!")
+  }
+
+}
+
+makeAsyncRequest()
